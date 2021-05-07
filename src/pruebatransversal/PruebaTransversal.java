6502afa9ab7026c463e53f38583aa9d6164634fa
Acevedo Eze <ezeelkpo2016@gmail.com>
2021-05-07 16:23:54 -0300

    Insertando proyecto ULP

--- src/pruebatransversal/PruebaTransversal.java
+++ src/pruebatransversal/PruebaTransversal.java
@@ -1,9 +1,15 @@
 
 package pruebatransversal;
 
+import java.sql.Connection;
+import modelo.Conexion;
+
 public class PruebaTransversal {
 
     public static void main(String[] args) {
+        Conexion con = new Conexion ();
+        
+        Connection c = con.getConnection();
         
     }
     
