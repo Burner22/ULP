818364c16596b8a7710580f32151b56cd73b272d
Acevedo Eze <ezeelkpo2016@gmail.com>
2021-05-11 18:41:36 -0300

    Insertando proyecto ULP

--- src/pruebatransversal/modelo/CursadaData.java
+++ src/pruebatransversal/modelo/CursadaData.java
@@ -1,30 +1,77 @@
 
 package pruebatransversal.modelo;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import pruebatransversal.entidades.Alumno;
 import pruebatransversal.entidades.Cursada;
 import pruebatransversal.entidades.Materia;
 
 public class CursadaData {
-    Connection connection;
+    Connection con;
     Conexion conexion;
     
     public CursadaData(Conexion conexion){
-        connection = conexion.getConnection();
+        con = conexion.getConnection();
     }
     
     public void guardarCursada(Cursada cursada){
+        try {
+            String sql = "INSER INTO cursada (idAlumno,idMateria,nota) VALUES (?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
         
+            ps.setInt(1, cursada.getAlum().getIdAlumno());
+            ps.setInt(2, cursada.getMater().getIdMateria());
+            ps.setDouble(3, cursada.getNota());
+            
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                cursada.setIdCursada(rs.getInt(1));
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "No se pudo obtener el Id de cursada");
+            }
+            ps.close();
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public ArrayList <Cursada> obtenerCursadas(){
+        ArrayList <Cursada> cursadas = new ArrayList<>();
+        Cursada cursada = null;
+        String sql = "SELECT * FROM cursada";
         
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()){
+                cursada = new Cursada();
+                cursada.setIdCursada(rs.getInt(1));
+                cursada.getAlum().setIdAlumno(rs.getInt(2));
+                cursada.getMater().setIdMateria(rs.getInt(3));
+                cursada.setNota(rs.getDouble(4));
+                cursadas.add(cursada);
+            }
+        
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cursadas;
     }
-
+/*
     public ArrayList <Cursada> obtenerCursadasXAlumno(int id){
-        
+         
     }
     
     public Alumno buscarAlumno (int id){
@@ -41,8 +88,9 @@ public class CursadaData {
     
     public ArrayList <Materia> obtenerMateriasNOCursadas (int id){
         
+        
     }
-    
+    */
     public void borrarCursadaDeUnaMateriaDeUnAlumno (int idAlumno, int idMateria){
         
     }
