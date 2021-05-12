d85ab93ed58936d31fcbc3bd14ee906b38b6554c
Acevedo Eze <ezeelkpo2016@gmail.com>
2021-05-12 15:56:56 -0300

    Vamos avanzando

--- src/pruebatransversal/modelo/CursadaData.java
+++ src/pruebatransversal/modelo/CursadaData.java
@@ -20,25 +20,37 @@ public class CursadaData {
     
     public void guardarCursada(Cursada cursada){
         try {
-            String sql = "INSER INTO cursada (idAlumno,idMateria,nota) VALUES (?,?,?)";
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        
-            ps.setInt(1, cursada.getAlum().getIdAlumno());
-            ps.setInt(2, cursada.getMater().getIdMateria());
-            ps.setDouble(3, cursada.getNota());
+            String cons = "SELECT * FROM cursada WHERE idAlumno=?,idMateria=?";
+            PreparedStatement ps1 = con.prepareStatement(cons, Statement.RETURN_GENERATED_KEYS);
+            ps1.setInt(1, cursada.getAlum().getIdAlumno());
+            ps1.setInt(2, cursada.getMater().getIdMateria());
+            ps1.executeQuery();
+            ResultSet rs1 = ps1.getGeneratedKeys();
             
-            ps.executeUpdate();
+            if(!rs1.next()){
             
-            ResultSet rs = ps.getGeneratedKeys();
-            
-            if(rs.next()){
-                cursada.setIdCursada(rs.getInt(1));
+                String sql = "INSER INTO cursada (idAlumno,idMateria,nota) VALUES (?,?,?)";
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+                ps.setInt(1, cursada.getAlum().getIdAlumno());
+                ps.setInt(2, cursada.getMater().getIdMateria());
+                ps.setDouble(3, cursada.getNota());
+
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+
+                if(rs.next()){
+                    cursada.setIdCursada(rs.getInt(1));
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "No se pudo obtener el Id de cursada");
+                }
+                ps.close();
             }
             else{
-                JOptionPane.showMessageDialog(null, "No se pudo obtener el Id de cursada");
+                JOptionPane.showMessageDialog(null, "No se pudo realizar la incripcion");
             }
-            ps.close();
-        
         } catch (SQLException ex) {
             Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -63,39 +75,125 @@ public class CursadaData {
                 cursadas.add(cursada);
             }
         
-        
+            ps.close();
         } catch (SQLException ex) {
             Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
         }
         return cursadas;
     }
-/*d
+
     public ArrayList <Cursada> obtenerCursadasXAlumno(int id){
-         
+        String sql = "SELECT * FROM cursada WHERE idAlumno=?";
+        ArrayList <Cursada> cursadas = new ArrayList <> ();
+        Cursada ins;      
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()){
+                ins = new Cursada();
+                ins.setIdCursada(rs.getInt(1));
+                Alumno alu = buscarAlumno(rs.getInt(2));
+                Materia mat = buscarMateria(rs.getInt(3));
+                ins.setAlum(alu);
+                ins.setMater(mat);
+                ins.setNota(rs.getDouble(4));
+                cursadas.add(ins);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
+        }     
+    return cursadas;    
     }
     
-    public Alumno buscarAlumno (int id){
+    public ArrayList <Cursada> obtenerMateriasCursadas (int id){
+    String sql = "SELECT * FROM cursada WHERE idMateria=?";
+    ArrayList <Cursada> materias = new ArrayList <>();   
+    Cursada ins;
+    try {
+        PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
         
+        while (rs.next()){
+            ins = new Cursada();
+            ins.setIdCursada(rs.getInt(1));
+            Alumno alu = buscarAlumno(rs.getInt(2));
+            Materia mat = buscarMateria(rs.getInt(3));
+            ins.setAlum(alu);
+            ins.setMater(mat);
+            ins.setNota(rs.getDouble(4));
+            materias.add(ins);
+        }
+        ps.close();
+    }catch (SQLException ex) {
+        Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return materias;
     }
     
-    public Materia buscarMateria (int id){
-        
+    public ArrayList <Materia> obtenerMateriasNOCursadas (){
+        Materia mater = null;
+        String sql = "SELECT * FROM materia WHERE NOT EXISTS (SELECT NULL FROM cursada WHERE materia.idMateria = cursada.idMateria)";
+        ArrayList <Materia> noCurs = new ArrayList <>();
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                mater = new Materia ();
+                mater.setIdMateria(rs.getInt(1));
+                mater.setNombre(rs.getString(2));
+                mater.setAño(rs.getInt(3));
+                mater.setEstado(rs.getBoolean(4));
+                noCurs.add(mater);
+            }
+         ps.close();   
+        } catch (SQLException ex) {
+            Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return noCurs;
     }
-    
-    public ArrayList <Materia> obtenerMateriasCursadas (int id){
-        
-    }
-    
-    public ArrayList <Materia> obtenerMateriasNOCursadas (int id){
-        
-        
-    }
-    */
+
     public void borrarCursadaDeUnaMateriaDeUnAlumno (int idAlumno, int idMateria){
+        String sql = "DELETE FROM cursada WHERE idAlumno=?,idMateria=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, idAlumno);
+            ps.setInt(2, idMateria);
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
     public void actualizarNotaCursada(int idAlumno, int nota){
+        String sql = "UPDATE cursada SET nota=? WHERE idAlumno=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, nota);
+            ps.setInt(2, idAlumno);
+            
+            ps.executeUpdate();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CursadaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
+    }
+
+    public Alumno buscarAlumno (int id){
+        AlumnoData newAlu = new AlumnoData(conexion);
+        return newAlu.buscarAlumno(id);
+    }
+    
+    public Materia buscarMateria (int id){
+        MateriaData newMate = new MateriaData(conexion);
+        return newMate.buscarMateria(id);
     }
 }
--- src/pruebatransversal/modelo/MateriaData.java
+++ src/pruebatransversal/modelo/MateriaData.java
@@ -1,5 +1,6 @@
 package pruebatransversal.modelo;
 
+import java.awt.HeadlessException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import pruebatransversal.entidades.Alumno;
 import pruebatransversal.entidades.Materia;
 
 public class MateriaData {
@@ -46,6 +48,39 @@ public class MateriaData {
             JOptionPane.showMessageDialog(null, "No se pudo agregar la Materia");
         }
     }
+    
+    public Materia buscarMateria (int id){
+        Materia materia = null;  //Instancio Alumno vacio para poder llenarlo y convertirlo en algo potable
+        
+        try{
+            materia = new Materia(); //Nuevo alumno por si se realiza otra busqueda
+            
+            String sql = "SELECT * FROM materia WHERE idMateria = ?";
+            
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1, id);   //(Parameter Index, valor int), 
+            
+            ResultSet rs = ps.executeQuery();   
+            
+            //Si encuentro alumno entonces lo instancio de manera manual, trayendo los datos de la BBDD
+            if (rs.next()){                         //LLeno el nuevo alumno con los valores que estan en la base de datos
+                materia.setIdMateria(rs.getInt(1));
+                materia.setNombre(rs.getString(2));
+                materia.setAño(rs.getInt(3));
+                materia.setEstado(rs.getBoolean(4));
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "No se ha encontrado dicha materia");
+            }
+            ps.close();
+        }
+        catch(HeadlessException | SQLException e){
+            JOptionPane.showMessageDialog(null, "No se ha encontrado alumno");
+        } 
+        
+        return materia;   
+    }
        
     public ArrayList <Materia> obtenerMaterias(){
         ArrayList <Materia> materias = new ArrayList <>();
@@ -81,7 +116,6 @@ public class MateriaData {
              ps.setInt(2, materia.getAño()); 
              ps.setBoolean(3, materia.getEstado());
                  
-             
              ps.executeUpdate(); 
                    
          } catch (SQLException ex) {
